d7b95302fa1da5d8de48bddf592c76d8fe8a0b73
alfredo <alfredo-49@hotmail.com>
2013-10-03 01:42:10 -0500

    prueba 1

---
+++ src/p1/P1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1;
+
+/**
+ *
+ * @author alfredo
+ */
+public class P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+        System.out.println("2 cambio");
+    }
+}
